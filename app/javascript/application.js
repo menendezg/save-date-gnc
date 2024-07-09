ae540f2833295710e7f52059be01c71ebfdbf4c1
Gabriel Menendez <menendez.gabriel@gmail.com>
2024-07-09 00:41:32 -0300

    commit after run rails stimulus install

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,4 @@
 // Entry point for the build script in your package.json
 import "./assets/stylesheets/application.tailwind.css"
 import "@hotwired/turbo-rails"
-import "./controllers"
+import "./controllers"import "./controllers"
