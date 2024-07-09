a9ea69e1d459becd824ebc912325685b5d4ba087
Gabriel Menendez <menendez.gabriel@gmail.com>
2024-07-08 22:55:22 -0300

    Add first commit for wedding website

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,5 @@
+class MainController < ApplicationController
+  def show
+    @couple = "Camila y Gabriel"
+  end
+end
---
+++ app/javascript/application.js
@@ -0,0 +1,4 @@
+// Entry point for the build script in your package.json
+import "./assets/stylesheets/application.tailwind.css"
+import "@hotwired/turbo-rails"
+import "./controllers"
